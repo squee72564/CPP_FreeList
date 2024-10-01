353de5466468482333e1c5a0c98bde1c57879b7f
squee72564 <rodriguez.al183@gmail.com>
2024-09-30 22:22:16 -0700

    Fixed formatting errors

--- include/FreeList.hpp
+++ include/FreeList.hpp
@@ -5,8 +5,6 @@
 #include <iterator>
 #include <limits>
 #include <cstddef>
-#include <algorithm>
-#include <exception>
 
 template<typename T>
 class FreeList {
@@ -22,10 +20,10 @@ private:
         Node() : data(T{}), next(SIZE_MAX), prev(SIZE_MAX), nextFree(SIZE_MAX) {}
         ~Node() = default;
 
-	Node(const Node&) = default;
-	Node(Node&&) noexcept = default;
-	Node& operator=(const Node&) = default;
-	Node& operator=(Node&&) noexcept = default;
+        Node(const Node&) = default;
+        Node(Node&&) noexcept = default;
+        Node& operator=(const Node&) = default;
+        Node& operator=(Node&&) noexcept = default;
     };
 
     std::vector<Node> nodes;
@@ -89,83 +87,84 @@ private:
         nodes[index].nextFree = freeHead;
         freeHead = index;
 
-	size_--;
+        size_--;
     }
 
     template <typename Compare = std::less<T> >
     size_t merge(const size_t first, const size_t second, size_t& tail, const Compare& comp = Compare()) {
-	if (first == SIZE_MAX) return second;
-	if (second == SIZE_MAX) return first;
+        if (first == SIZE_MAX) return second;
+        if (second == SIZE_MAX) return first;
 
-	size_t result = SIZE_MAX;
+        size_t result = SIZE_MAX;
 
-	if (comp(nodes[first].data, nodes[second].data)) {
-	    result = first;
+        if (comp(nodes[first].data, nodes[second].data)) {
+            result = first;
 
-	    nodes[first].next = merge(nodes[first].next, second, tail, comp);
-	    if (nodes[first].next != SIZE_MAX) {
-		nodes[nodes[first].next].prev = first;
-	    } else {
-		tail = first;
-	    }
-	    nodes[first].prev = SIZE_MAX;
-	} else {
-	    result = second;
+            nodes[first].next = merge(nodes[first].next, second, tail, comp);
+            if (nodes[first].next != SIZE_MAX) {
+                nodes[nodes[first].next].prev = first;
+            } else {
+                tail = first;
+            }
+            nodes[first].prev = SIZE_MAX;
+        } else {
+            result = second;
 
-	    nodes[second].next = merge(first, nodes[second].next, tail, comp);
-	    if (nodes[second].next != SIZE_MAX) {
-		nodes[nodes[second].next].prev = second;
-	    } else {
-		tail = second;
-	    }
-	    nodes[second].prev = SIZE_MAX;
-	}
-	return result;
+            nodes[second].next = merge(first, nodes[second].next, tail, comp);
+            if (nodes[second].next != SIZE_MAX) {
+                nodes[nodes[second].next].prev = second;
+            } else {
+                tail = second;
+            }
+            nodes[second].prev = SIZE_MAX;
+        }
+        return result;
     }
 
     std::pair<size_t,size_t> split(const size_t start, const size_t end) {
-	if (start == end) return {start, start};
+        if (start == end) return {start, start};
 
-	size_t slow = start;
-	size_t fast = start;
+        size_t slow = start;
+        size_t fast = start;
 
-	while (fast != end && nodes[fast].next != end) {
-	    slow = nodes[slow].next;
-	    fast = nodes[nodes[fast].next].next;
-	}
+        while (fast != end && nodes[fast].next != end) {
+            slow = nodes[slow].next;
+            fast = nodes[nodes[fast].next].next;
+        }
 
-	size_t second_half = nodes[slow].next;
-	nodes[slow].next = SIZE_MAX;
+        size_t second_half = nodes[slow].next;
+        nodes[slow].next = SIZE_MAX;
 
-	if (second_half != SIZE_MAX) {
-	    nodes[second_half].prev = SIZE_MAX;
-	}
-	return {second_half,slow};
+        if (second_half != SIZE_MAX) {
+            nodes[second_half].prev = SIZE_MAX;
+        }
+        return {second_half,slow};
     }
 
     template <typename Compare = std::less<T> >
     size_t mergeSort(size_t start, size_t end, const Compare& comp = Compare()) {
-	if (start == SIZE_MAX || start == end) return start;
+        if (start == SIZE_MAX || start == end) return start;
 
-	auto [second_half_start, start_end] = split(start, end);
+        auto [second_half_start, start_end] = split(start, end);
 
-	start = mergeSort(start, start_end, comp);
-	second_half_start = mergeSort(second_half_start, end, comp);
+        start = mergeSort(start, start_end, comp);
+        second_half_start = mergeSort(second_half_start, end, comp);
 
-	size_t _tail = SIZE_MAX;
-	size_t _head = merge(start, second_half_start, _tail, comp);
-	tail = _tail;
+        size_t _tail = SIZE_MAX;
+        size_t _head = merge(start, second_half_start, _tail, comp);
+        tail = _tail;
 
-	return _head;
+        return _head;
     }
 
 public:
+    using value_type = T;
 
     class Iterator {
     public:
         using iterator_category = std::bidirectional_iterator_tag;
-        using value_type = T;
         using difference_type = std::ptrdiff_t;
+        using value_type = T;
         using pointer = T*;
         using reference = T&;
 
@@ -177,10 +176,10 @@ public:
         Iterator(const FreeList* list, size_t index)
             : list(const_cast<FreeList*>(list)), index(index) {}
 
-	Iterator(const Iterator&) = default;
-	Iterator(Iterator&&) noexcept = default;
-	Iterator& operator=(const Iterator&) = default;
-	Iterator& operator=(Iterator&&) noexcept = default;
+        Iterator(const Iterator&) = default;
+        Iterator(Iterator&&) noexcept = default;
+        Iterator& operator=(const Iterator&) = default;
+        Iterator& operator=(Iterator&&) noexcept = default;
 
         reference operator*() {
             return list->nodes[index].data;
@@ -197,22 +196,22 @@ public:
 
         Iterator operator++(int) {
             Iterator temp = *this;
-	    ++(*this);
+            ++(*this);
             return temp;
         }
 
         Iterator& operator--() {
 	    if (index == SIZE_MAX) {
-		index = list->tail;
+            index = list->tail;
 	    } else {
-		index = list->nodes[index].prev;
+            index = list->nodes[index].prev;
 	    }
             return *this;
         }
 
         Iterator operator--(int) {
             Iterator temp = *this;
-	    --(*this);
+            --(*this);
             return temp;
         }
 
@@ -236,8 +235,8 @@ public:
     class ConstIterator {
     public:
 	using iterator_category = std::bidirectional_iterator_tag;
-	using value_type = T;
 	using difference_type = std::ptrdiff_t;
+	using value_type = T;
 	using pointer = const T*;
 	using reference = const T&;
 
@@ -275,9 +274,9 @@ public:
 
 	ConstIterator& operator--() {
 	    if (index == SIZE_MAX) {
-		index = list->tail;
+            index = list->tail;
 	    } else {
-		index = list->nodes[index].prev;
+            index = list->nodes[index].prev;
 	    }
 	    return *this;
 	}
@@ -301,35 +300,61 @@ public:
 	}
 
     private:
-	const FreeList* list; // Pointer to const FreeList
-	size_t index;
+        const FreeList* list;
+        size_t index;
     };
+
+    using iterator = Iterator;
+    using const_iterator = ConstIterator;
+    using reverse_iterator = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+    iterator begin() { return iterator(this, head); }
+    const_iterator begin() const { return const_iterator(this, head); }
+    const_iterator cbegin() const noexcept { return const_iterator(this, head); }
+
+    iterator end() { return iterator(this, SIZE_MAX); }
+    const_iterator end() const { return const_iterator(this, SIZE_MAX); }
+    const_iterator cend() const noexcept { return const_iterator(this, SIZE_MAX); }
+
+    reverse_iterator rbegin() { return reverse_iterator(end()); }
+    const_reverse_iterator rbegin() const { return const_reverse_iterator(end()); }
+    const_reverse_iterator crbegin() const noexcept { return const_reverse_iterator(cend()); }
+
+    reverse_iterator rend() { return reverse_iterator(begin()); }
+    const_reverse_iterator rend() const { return const_reverse_iterator(begin()); }
+    const_reverse_iterator crend() const noexcept { return const_reverse_iterator(cbegin()); }
 
     FreeList() : head(SIZE_MAX), tail(SIZE_MAX), freeHead(SIZE_MAX) {}
 
-    FreeList(size_t count, const T& value = T()) : FreeList() {
-	for (size_t i = 0; i < count; ++i) {
-	    push_back(value);
-	}
+    FreeList(size_t count) : FreeList() {
+        for (size_t i = 0; i < count; ++i) {
+            push_back(T{});
+        }
     }
 
-    explicit FreeList(size_t count) : FreeList() {
-	for (size_t i = 0; i < count; ++i) {
-	    push_back(T());
-	}
+    FreeList(size_t count, const T& value) : FreeList() {
+        for (size_t i = 0; i < count; ++i) {
+            push_back(value);
+        }
     }
 
-    template <class InputIt>
-    FreeList(InputIt first, InputIt last) : FreeList() {
-	for (auto it = first; it != last; ++it) {
-	    push_back(*it);
-	}
+    FreeList(iterator first, iterator last) : FreeList() {
+        for (auto it = first; it != last; ++it) {
+            push_back(*it);
+        }
+    }
+
+    FreeList(const_iterator first, const_iterator last) : FreeList() {
+        for (auto it = first; it != last; ++it) {
+            push_back(*it);
+        }
     }
 
     FreeList(std::initializer_list<T> init) : FreeList() {
-	for (const auto& value : init) {
-	    push_back(value);
-	}
+        for (const auto& value : init) {
+            push_back(value);
+        }
     }
 
     ~FreeList() = default;
@@ -341,19 +366,25 @@ public:
 
     template <typename Compare = std::less<T> >
     void sort(const Compare& comp = Compare()) {
-	if (empty()) return;	
+        if (empty()) return;
 
-	head = mergeSort(head, tail, comp);
+        head = mergeSort(head, tail, comp);
     }
 
     template <typename Compare = std::less<T> >
-    void sort(const Iterator start = begin(), const Iterator _end = end(), const Compare& comp = Compare()) {
-	if (empty() || start == end() || start == _end) return;	
+    void sort(const iterator start = iterator(),
+	      const iterator _end = iterator(),
+	      const Compare& comp = Compare())
+    {
+        if (empty() || start == end() || start == _end) return;
 
-	size_t start_idx = start.getIndex();
-	size_t end_idx = (_end == end()) ? tail : _end.getIndex();
+        iterator actualStart = (start == iterator()) ? begin() : start;
+        iterator actualEnd = (_end == iterator()) ? end() : _end;
 
-	head = mergeSort(start_idx, end_idx, comp);
+        size_t start_idx = actualStart.getIndex();
+        size_t end_idx = (actualEnd == end()) ? tail : _end.getIndex();
+
+        head = mergeSort(start_idx, end_idx, comp);
     }
 
     void reserve(size_t count) {
@@ -368,7 +399,9 @@ public:
             nodes[index].next = head;
             nodes[head].prev = index;
         }
+
         head = index;
+
         if (tail == SIZE_MAX) {
             tail = index;
         }
@@ -389,29 +422,28 @@ public:
     }
 
     template<class... Args>
-    Iterator emplace(ConstIterator pos, Args&&... args) {
+    iterator emplace(const_iterator pos, Args&&... args) {
 
-	if (pos == end()) {
+        if (pos == end()) {
+            return insert(end(), T(std::forward<Args>(args)...));
+        }
 
-	    return insert(end(), T(std::forward<Args>(args)...));
-	}
+        size_t currentIndex = pos.getIndex();
 
-	size_t currentIndex = pos.getIndex();
-	
-	size_t newIndex = allocateNode(T(std::forward<Args>(args)...));
+        size_t newIndex = allocateNode(T(std::forward<Args>(args)...));
 
-	nodes[newIndex].next = currentIndex;
-	nodes[newIndex].prev = nodes[currentIndex].prev;
+        nodes[newIndex].next = currentIndex;
+        nodes[newIndex].prev = nodes[currentIndex].prev;
 
-	if (nodes[currentIndex].prev != SIZE_MAX) {
-	    nodes[nodes[currentIndex].prev].next = newIndex;
-	} else {
-	    head = newIndex;
-	}
-	
-	nodes[currentIndex].prev = newIndex;
+        if (nodes[currentIndex].prev != SIZE_MAX) {
+            nodes[nodes[currentIndex].prev].next = newIndex;
+        } else {
+            head = newIndex;
+        }
 
-	return Iterator(this, newIndex);
+        nodes[currentIndex].prev = newIndex;
+
+        return iterator(this, newIndex);
     }
 
     template<typename... Args>
@@ -431,7 +463,7 @@ public:
 
     template<typename... Args>
     T& emplace_back(Args&&... args) {
-	size_t index = allocateNode(T(std::forward<Args>(args)...));
+        size_t index = allocateNode(T(std::forward<Args>(args)...));
 
         if (head == SIZE_MAX) {
             head = index;
@@ -442,12 +474,12 @@ public:
             tail = index;
         }
 
-	return nodes[index].data;
+        return nodes[index].data;
     }
 
     template<typename... Args>
     void emplace_back(Args&&... args) {
-	size_t index = allocateNode(T(std::forward<Args>(args)...));
+        size_t index = allocateNode(T(std::forward<Args>(args)...));
 
         if (head == SIZE_MAX) {
             head = index;
@@ -459,47 +491,47 @@ public:
         }
     }
 
-    Iterator erase(Iterator pos) {
-	Iterator next = pos;
-	++next;
-	remove(pos.getIndex());
-	return next;
+    iterator erase(iterator pos) {
+        iterator next = pos;
+        ++next;
+        remove(pos.getIndex());
+        return next;
     }
 
-    ConstIterator erase(ConstIterator pos) {
-	ConstIterator next = pos;
-	++next;
-	remove(pos.getIndex());
-	return next;
+    const_iterator erase(const_iterator pos) {
+        const_iterator next = pos;
+        ++next;
+        remove(pos.getIndex());
+        return next;
     }
 
-    Iterator erase(Iterator first, Iterator last) {
-	while (first != last) {
-	    first = erase(first);
-	}
+    iterator erase(iterator first, iterator last) {
+        while (first != last) {
+            first = erase(first);
+        }
 
-	return last;
+        return last;
     }
 
-    ConstIterator erase(ConstIterator first, ConstIterator last) {
-	while (first != last) {
-	    first = erase(first);
-	}
+    const_iterator erase(const_iterator first, const_iterator last) {
+        while (first != last) {
+            first = erase(first);
+        }
 
-	return last;
+        return last;
     }
 
-    Iterator find(const T& value) {
-	for (Iterator it = begin(); it != end(); ++it) {
-	    if (*it == value) {
-		return it;
-	    }
-	}
-	return end();
+    iterator find(const T& value) {
+        for (iterator it = begin(); it != end(); ++it) {
+            if (*it == value) {
+            return it;
+            }
+        }
+        return end();
     }
 
     template <typename U>
-    Iterator insert(Iterator it, U&& data) {
+    iterator insert(iterator it, U&& data) {
         size_t newIndex = allocateNode(std::forward<U>(data));
     
         if (!(it == end())) {
@@ -526,10 +558,10 @@ public:
             tail = newIndex;
         }
     
-        return Iterator(this, newIndex);
+        return iterator(this, newIndex);
     }
 
-    Iterator insert(Iterator it, const T& data) {
+    iterator insert(iterator it, const T& data) {
         size_t newIndex = allocateNode(data);
     
         if (!(it == end())) {
@@ -556,59 +588,57 @@ public:
             tail = newIndex;
         }
     
-        return Iterator(this, newIndex);
+        return iterator(this, newIndex);
     }
 
     template<class InputIt>
-    Iterator insert(Iterator pos, InputIt first, InputIt last) {
-	size_t currentIndex = pos.getIndex();
-	size_t firstNewIndex = SIZE_MAX;
+    iterator insert(iterator pos, InputIt first, InputIt last) {
+        size_t currentIndex = pos.getIndex();
+        size_t firstNewIndex = SIZE_MAX;
 
-	while (first != last) {
-	    size_t newIndex = allocateNode(*first++);
-	    
-	    if (firstNewIndex == SIZE_MAX) {
-		firstNewIndex = newIndex;
-	    }
+        while (first != last) {
+            size_t newIndex = allocateNode(*first++);
 
-	    // Insert the new node in front of currentIndex
-	    if (currentIndex != SIZE_MAX) {
-		nodes[newIndex].next = currentIndex;
-		nodes[newIndex].prev = nodes[currentIndex].prev;
+            if (firstNewIndex == SIZE_MAX) {
+                firstNewIndex = newIndex;
+            }
 
-		if (nodes[currentIndex].prev != SIZE_MAX) {
-		    nodes[nodes[currentIndex].prev].next = newIndex;
-		} else {
-		    head = newIndex;
-		}
+            if (currentIndex != SIZE_MAX) {
+                nodes[newIndex].next = currentIndex;
+                nodes[newIndex].prev = nodes[currentIndex].prev;
 
-		nodes[currentIndex].prev = newIndex;
-		currentIndex = newIndex; // Update for the next insertion
-	    } else {
-		// Inserting at the end
-		if (tail != SIZE_MAX) {
-		    nodes[tail].next = newIndex;
-		    nodes[newIndex].prev = tail;
-		} else {
-		    head = newIndex; // List was empty
-		}
-		tail = newIndex;
-	    }
-	}
+                if (nodes[currentIndex].prev != SIZE_MAX) {
+                    nodes[nodes[currentIndex].prev].next = newIndex;
+                } else {
+                    head = newIndex;
+                }
 
-	return Iterator(this, firstNewIndex);
+                nodes[currentIndex].prev = newIndex;
+                currentIndex = newIndex;
+            } else {
+                if (tail != SIZE_MAX) {
+                    nodes[tail].next = newIndex;
+                    nodes[newIndex].prev = tail;
+                } else {
+                    head = newIndex;
+                }
+                tail = newIndex;
+            }
+        }
+
+        return iterator(this, firstNewIndex);
     }
 
-    Iterator insert(Iterator pos, std::initializer_list<T> ilist) {
-	return insert(pos, ilist.begin(), ilist.end());
+    iterator insert(iterator pos, std::initializer_list<T> ilist) {
+        return insert(pos, ilist.begin(), ilist.end());
     }
 
     void swap(FreeList& other) noexcept {
-	std::swap(head, other.head);
-	std::swap(tail, other.tail);
-	std::swap(freeHead, other.freeHead);
-	nodes.swap(other.nodes);
-	std::swap(size_, other.size_);
+        std::swap(head, other.head);
+        std::swap(tail, other.tail);
+        std::swap(freeHead, other.freeHead);
+        nodes.swap(other.nodes);
+        std::swap(size_, other.size_);
     }
 
     const T& front() const {
@@ -646,41 +676,22 @@ public:
     }
 
     size_t size() const noexcept {
-	return size_;
+        return size_;
     }
 
     size_t capacity() const noexcept {
-	return nodes.capacity();
+        return nodes.capacity();
     }
 
     void shrink_to_fit() {
-	nodes.shrink_to_fit();
+        nodes.shrink_to_fit();
     }
 
     void clear() {
         head = tail = freeHead = SIZE_MAX;
-	size_ = 0;
+        size_ = 0;
         nodes.clear();
     }
-
-    using reverse_iterator = std::reverse_iterator<Iterator>;
-    using const_reverse_iterator = std::reverse_iterator<ConstIterator>;
-
-    Iterator begin() { return Iterator(this, head); }
-    ConstIterator begin() const { return ConstIterator(this, head); }
-    ConstIterator cbegin() const noexcept { return ConstIterator(this, head); }
-
-    Iterator end() { return Iterator(this, SIZE_MAX); }
-    ConstIterator end() const { return ConstIterator(this, SIZE_MAX); }
-    ConstIterator cend() const noexcept { return ConstIterator(this, SIZE_MAX); }
-
-    reverse_iterator rbegin() { return reverse_iterator(end()); }
-    const_reverse_iterator rbegin() const { return const_reverse_iterator(end()); }
-    const_reverse_iterator crbegin() const noexcept { return const_reverse_iterator(cend()); }
-
-    reverse_iterator rend() { return reverse_iterator(begin()); }
-    const_reverse_iterator rend() const { return const_reverse_iterator(begin()); }
-    const_reverse_iterator crend() const noexcept { return const_reverse_iterator(cbegin()); }
 };
 
 #endif
